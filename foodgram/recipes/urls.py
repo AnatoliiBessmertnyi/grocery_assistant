be8650de561882d9c4d996fc880887b38f704d7f
Anatolii <noob1917.ru@gmail.com>
2023-09-10 15:14:05 +0300

    первый вьюсет и роутер

--- foodgram/recipes/urls.py
+++ foodgram/recipes/urls.py
@@ -1,9 +1,26 @@
 
-from django.urls import path
+# from django.urls import path
 
-from .views import api_recipes, api_recipes_detail
+# from .views import RecipeList, RecipeDetail# APIRecipe # api_recipes, api_recipes_detail
+
+# urlpatterns = [
+#     path('api/recipes/', RecipeList.as_view(), name='api_recipes'),
+#     path(
+#         'api/recipes/<int:pk>/', RecipeDetail.as_view(), name='api_recipes_pk'
+#     ),
+    # path('api/recipes/', api_recipes, name='api_recipes'),
+    # path('api/recipes/<int:pk>/', api_recipes_detail, name='api_recipes_pk'),
+# ]
+
+from rest_framework.routers import DefaultRouter
+
+from django.urls import include, path
+
+from recipes.views import RecipeViewSet
+
+router = DefaultRouter()
+router.register('api/recipes', RecipeViewSet, basename='recipes')
 
 urlpatterns = [
-    path('api/recipes/', api_recipes, name='api_recipes'),
-    path('api/recipes/<int:pk>/', api_recipes_detail, name='api_recipes_pk'),
-]
+    path('', include(router.urls)),
+] 
--- foodgram/recipes/views.py
+++ foodgram/recipes/views.py
@@ -1,34 +1,70 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
+# from rest_framework.decorators import api_view
+# from rest_framework.views import APIView
+# from rest_framework import generics
+# from rest_framework.response import Response
+# from rest_framework import status
+from rest_framework import viewsets
 
 from .models import Recipe
 from .serializers import RecipeSerializer
 
+# Вью функции
 
-@api_view(['GET', 'POST'])
-def api_recipes(request):
-    if request.method == 'POST':
-        serializer = RecipeSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    recipes = Recipe.objects.all()
-    serializer = RecipeSerializer(recipes, many=True)
-    return Response(serializer.data)
+# @api_view(['GET', 'POST'])
+# def api_recipes(request):
+#     if request.method == 'POST':
+#         serializer = RecipeSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     recipes = Recipe.objects.all()
+#     serializer = RecipeSerializer(recipes, many=True)
+#     return Response(serializer.data)
 
-@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
-def api_recipes_detail(request, pk):
-    recipe = Recipe.objects.get(pk=pk)
-    if request.method == 'PUT' or request.method == 'PATCH':
-        serializer = RecipeSerializer(recipe, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        recipe.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    serializer = RecipeSerializer(recipe)
-    return Response(serializer.data)
+# @api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+# def api_recipes_detail(request, pk):
+#     recipe = Recipe.objects.get(pk=pk)
+#     if request.method == 'PUT' or request.method == 'PATCH':
+#         serializer = RecipeSerializer(recipe, data=request.data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     elif request.method == 'DELETE':
+#         recipe.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+#     serializer = RecipeSerializer(recipe)
+#     return Response(serializer.data)
+
+# Вью классы низкоуровневые
+
+# class APIRecipe(APIView):
+#     def get(self, request):
+#         recipes = Recipe.objects.all()
+#         serializer = RecipeSerializer(recipes, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request):
+#         serializer = RecipeSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED) 
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Высокоуровневые дженерики
+
+# class RecipeList(generics.ListCreateAPIView):
+#     queryset = Recipe.objects.all()
+#     serializer_class = RecipeSerializer
+
+
+# class RecipeDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Recipe.objects.all()
+#     serializer_class = RecipeSerializer 
+
+# Высокоуровневые вьюсеты
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer 

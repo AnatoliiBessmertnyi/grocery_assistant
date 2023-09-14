f39b02f747065a9fe0ffd89779e9e373b3f29695
Anatolii <noob1917.ru@gmail.com>
2023-09-14 16:44:31 +0300

    настроены изображения и обновление данных о рецептах

--- foodgram/api/serializers.py
+++ foodgram/api/serializers.py
@@ -1,4 +1,7 @@
+import base64
+
 from rest_framework import serializers
+from django.core.files.base import ContentFile
 
 from recipes.models import (
     Recipe, Ingredient, IngredientRecipe, Tag, TagRecipe, User
@@ -30,15 +33,27 @@ class IngredientSerializer(serializers.ModelSerializer):
         fields = ('id', 'ingredient_name')
 
 
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+
+            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+
+        return super().to_internal_value(data)
+
+
 class RecipeSerializer(serializers.ModelSerializer):
     # tags = TagSerializer(required=False)
     ingredients = IngredientSerializer(many=True)
+    image = Base64ImageField(required=False, allow_null=True)
 
     class Meta:
         model = Recipe
         fields = (
             'name', 'cooking_time', 'ingredients', 'description',
-            'pub_date', 'author', 'tags', 
+            'pub_date', 'author', 'tags', 'image'
         )
         read_only_fields = ('author',)
 
@@ -68,6 +83,22 @@ class RecipeSerializer(serializers.ModelSerializer):
                 ingredient=current_ingredient, recipe=recipe
             )
         return recipe
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.image = validated_data.get('image', instance.image)
+        if 'ingredients' in validated_data:
+            ingredients_data = validated_data.pop('ingredients')
+            lst = []
+            for ingredient in ingredients_data:
+                current_ingredient, status = ingredient.objects.get_or_create(
+                    **ingredient
+                    )
+                lst.append(current_ingredient)
+            instance.ingredients.set(lst)
+
+        instance.save()
+        return instance
     
 
 
--- foodgram/api/urls.py
+++ foodgram/api/urls.py
@@ -3,10 +3,11 @@ from django.urls import include, path
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
-from .views import RecipeViewSet, UserViewSet
+from .views import RecipeViewSet, UserViewSet, IngredientViewSet
 
 router = DefaultRouter()
 router.register('recipes', RecipeViewSet, basename='recipes')
+router.register('ingredients', IngredientViewSet)
 router.register('users', UserViewSet)
 
 urlpatterns = [
